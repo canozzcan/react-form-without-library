f2dfa7fb777d75c0f6ace5d1558937eab08b5648
canozzcan <canozcanstu@gmail.com>
2022-08-15 19:12:25 +0300

    Validate React Forms without a Library

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./formInput.css";
+
+const FormInput = (props) => {
+  const [focus, setFocus] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocus(true);
+  };
+
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input {...inputProps} onChange={onChange} onBlur={handleFocus} focus={focus.toString()}/>
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FormInput;
